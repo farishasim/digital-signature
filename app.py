306f51491f3c2376cbf594102838b4e15a8e61b0
leo.matt.547 <leo.matt.547@gmail.com>
2021-11-17 23:02:10 +0700

    download file backend done

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import *
+from backend import backend
 import os
 
 app = Flask(__name__)
@@ -20,11 +21,20 @@ def signFile():
 def verifyFile():
     return render_template("verify.html")
 
-@app.route('/download/<filename>')
-def download(filename):
+@app.route('/download/public')
+def download_public():
     path = os.path.join(current_app.root_path + "/" + app.config["UPLOAD_FOLDER"])
-    ext = filename.rsplit('.', 1)[1].lower()
-    return send_file(os.path.join(path,filename), as_attachment=True)
+    backend.get_public_key_file(os.path.join(path,"public.pub"))
+    #ext = filename.rsplit('.', 1)[1].lower()
+    return send_file(os.path.join(path,"public.pub"), as_attachment=True)
+
+@app.route('/download/private')
+def download_private():
+    #Program untuk generate key di sini
+    path = os.path.join(current_app.root_path + "/" + app.config["UPLOAD_FOLDER"])
+    backend.get_private_key_file(os.path.join(path,"private.pri"))
+    #ext = filename.rsplit('.', 1)[1].lower()
+    return send_file(os.path.join(path,"private.pri"), as_attachment=True)
     """
     if(ext in ALLOWED_EXTENSIONS_CITRA):
         return send_file(os.path.join(path,filename), as_attachment=True, mimetype='image/'+str(ext))
---
+++ backend/backend.py
@@ -0,0 +1,15 @@
+def get_public_key_file(filename):
+    #open text file
+    text_file = open(filename, "w")
+    #write public key
+    text_file.write('Hello World!')
+    #close file
+    text_file.close()
+
+def get_private_key_file(filename):
+    #open text file
+    text_file = open(filename, "w")
+    #write private key
+    text_file.write('Hello World!')
+    #close file
+    text_file.close()
