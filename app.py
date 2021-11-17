af65be1f192b4ad4e43f3152a975c8189ea3a6f0
leo.matt.547 <leo.matt.547@gmail.com>
2021-11-17 22:40:00 +0700

    adding download backend

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import *
+import os
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"]='dump'
 
 @app.route("/")
 def index():
@@ -17,6 +19,18 @@ def signFile():
 @app.route("/verify")
 def verifyFile():
     return render_template("verify.html")
+
+@app.route('/download/<filename>')
+def download(filename):
+    path = os.path.join(current_app.root_path + "/" + app.config["UPLOAD_FOLDER"])
+    ext = filename.rsplit('.', 1)[1].lower()
+    return send_file(os.path.join(path,filename), as_attachment=True)
+    """
+    if(ext in ALLOWED_EXTENSIONS_CITRA):
+        return send_file(os.path.join(path,filename), as_attachment=True, mimetype='image/'+str(ext))
+    elif(ext in ALLOWED_EXTENSIONS_VIDEO):
+        return send_file(os.path.join(path,filename), as_attachment=True, mimetype='video/'+str(ext))
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
